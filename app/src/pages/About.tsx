fb99d6e8c2368fd5dc9112f24f3623c33af08ed2
Wsko-12 <vaskovladislav@gmail.com>
2022-09-26 17:50:32 +0300

    feat: add pages and routing

---
+++ app/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <section>
+        <div>About Us</div>
+      </section>
+    );
+  }
+}
---
+++ app/src/pages/Page404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Component } from 'react';
+
+export default class Page404 extends Component {
+  render() {
+    return (
+      <section>
+        <div>404</div>
+      </section>
+    );
+  }
+}
