4f6db4e4e3b3ab534729c758183dfcb189e268cf
Wsko-12 <vaskovladislav@gmail.com>
2022-09-30 15:29:40 +0300

    feat: extend form input

--- app/src/components/MyForm/FormInput/FormInput.tsx
+++ app/src/components/MyForm/FormInput/FormInput.tsx
@@ -3,13 +3,18 @@ import styles from '../form.module.css';
 
 interface IFormInputProps {
   label?: string;
+  placeholder?: string;
+  type?: React.HTMLInputTypeAttribute;
 }
 
-export default function FormInput({ label = '' }: IFormInputProps) {
+export default function FormInput({ type = 'text', label = '', placeholder }: IFormInputProps) {
+  if (!placeholder) {
+    placeholder = label;
+  }
   return (
     <label className={styles.form__label}>
       {label}
-      <input className={styles.form__input} type="text" placeholder={label}></input>
+      <input className={styles.form__input} type={type} placeholder={placeholder}></input>
     </label>
   );
 }
--- app/src/components/MyForm/FormInput/__test__/FormInput.test.tsx
+++ app/src/components/MyForm/FormInput/__test__/FormInput.test.tsx
@@ -3,16 +3,29 @@ import React from 'react';
 import FormInput from '../FormInput';
 
 const label = 'test-label';
+const placeholder = 'test-placeholder';
 describe('FormInput', () => {
-  test('should have placeholder', () => {
+  test('should be rendered without label and placeholder', () => {
+    render(<FormInput />);
+    const result = screen.getByRole('textbox');
+    expect(result).toBeInTheDocument();
+  });
+
+  test('should render label if receive this prop', () => {
+    render(<FormInput label={label} />);
+    const result = screen.getByLabelText(label);
+    expect(result).toBeInTheDocument();
+  });
+
+  test('should render placeholder if receive label equals label', () => {
     render(<FormInput label={label} />);
     const result = screen.getByPlaceholderText(label);
     expect(result).toBeInTheDocument();
   });
 
-  test('should have label', () => {
-    render(<FormInput label={label} />);
-    const result = screen.getByLabelText(label);
+  test('should have placeholder different from label if receive it prop', () => {
+    render(<FormInput label={label} placeholder={placeholder} />);
+    const result = screen.getByPlaceholderText(placeholder);
     expect(result).toBeInTheDocument();
   });
 
@@ -23,5 +36,17 @@ describe('FormInput', () => {
     fireEvent.change(result, { target: { value } });
 
     expect(result.value).toBe(value);
+  });
+
+  test('should be text type by default', () => {
+    render(<FormInput />);
+    const result = screen.getByRole<HTMLInputElement>('textbox');
+    expect(result.type).toBe('text');
+  });
+
+  test('should apply type prop', () => {
+    render(<FormInput type="email" />);
+    const result = screen.getByRole<HTMLInputElement>('textbox');
+    expect(result.type).toBe('email');
   });
 });
