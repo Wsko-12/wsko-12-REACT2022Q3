d91dcc1e238dd0ca1990e047a0a7136dba5ed8bb
Wsko-12 <vaskovladislav@gmail.com>
2022-10-04 14:56:01 +0300

    fix: nav link active class

--- app/src/components/Header/HeaderNav/HeaderNav.tsx
+++ app/src/components/Header/HeaderNav/HeaderNav.tsx
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './header-nav.module.css';
 
+type classNameProps = {
+  isActive: boolean;
+  isPending: boolean;
+};
+
+const getClassName = ({ isActive }: classNameProps) => (isActive ? styles.active : undefined);
+
 export default class HeaderNav extends Component {
   render() {
     return (
       <nav className={styles['header-nav']}>
-        <NavLink end to="/" className={`${styles['header-nav__link']} ${styles.link}`}>
+        <NavLink end to="/" className={getClassName}>
           Main
         </NavLink>
-        <NavLink to="/about" className={`${styles['header-nav__link']} ${styles.link}`}>
+        <NavLink to="/about" className={getClassName}>
           About us
         </NavLink>
-        <NavLink to="/form" className={`${styles['header-nav__link']} ${styles.link}`}>
+        <NavLink to="/form" className={getClassName}>
           Form
         </NavLink>
       </nav>
