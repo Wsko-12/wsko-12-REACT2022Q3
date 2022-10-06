231485f05ca0df11ff0effadc6d0a52957dfa6ca
Wsko-12 <vaskovladislav@gmail.com>
2022-10-06 14:19:20 +0300

    feat: add requared to radioSwitcher

--- app/src/components/form/RadioSwitcher/RadioSwitcher.tsx
+++ app/src/components/form/RadioSwitcher/RadioSwitcher.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from '../form-components.module.css';
+import InputWithMessage from '../InputWithMessage/InputWithMessage';
 
 interface IRadioSwitcherProps {
   values: (string | [string, string])[];
@@ -7,28 +8,55 @@ interface IRadioSwitcherProps {
   label?: string;
 }
 
-export default function RadioSwitcher({ values, label, name }: IRadioSwitcherProps) {
-  return (
-    <>
-      <label className={styles.form__label}>{label}</label>
-      {values.map((value) => {
-        if (Array.isArray(value)) {
-          const [val, text] = value;
+interface IRadioSwitcherState {
+  isValid: boolean;
+}
+export default class RadioSwitcher extends Component<IRadioSwitcherProps, IRadioSwitcherState> {
+  constructor(props: IRadioSwitcherProps) {
+    super(props);
+    this.state = {
+      isValid: true,
+    };
+  }
+  render() {
+    const { values, name, label } = this.props;
+    const { isValid } = this.state;
+    return (
+      <InputWithMessage isValid={isValid} message="Please, choose one">
+        <label className={styles.form__label}>{label}</label>
+        {values.map((value) => {
+          if (Array.isArray(value)) {
+            const [val, text] = value;
+            return (
+              <label key={val} className={styles.form__label_checkbox}>
+                <input
+                  type="radio"
+                  name={name}
+                  value={val}
+                  required
+                  onChange={() => this.setState({ isValid: true })}
+                  onInvalid={() => this.setState({ isValid: false })}
+                />
+                {text}
+              </label>
+            );
+          }
+
           return (
-            <label key={val} className={styles.form__label_checkbox}>
-              <input type="radio" name={name} value={val} />
-              {text}
+            <label key={value} className={styles.form__label_checkbox}>
+              <input
+                type="radio"
+                name={name}
+                value={value.toLowerCase()}
+                required
+                onChange={() => this.setState({ isValid: true })}
+                onInvalid={() => this.setState({ isValid: false })}
+              />
+              {value}
             </label>
           );
-        }
-
-        return (
-          <label key={value} className={styles.form__label_checkbox}>
-            <input type="radio" name={name} value={value.toLowerCase()} />
-            {value}
-          </label>
-        );
-      })}
-    </>
-  );
+        })}
+      </InputWithMessage>
+    );
+  }
 }
