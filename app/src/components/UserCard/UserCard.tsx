7afae063935c23dcb68eaba9e40d09187166967e
Wsko-12 <vaskovladislav@gmail.com>
2022-10-07 13:38:14 +0300

    feat: userCard receive data

--- app/src/components/UserCard/UserCard.tsx
+++ app/src/components/UserCard/UserCard.tsx
@@ -8,6 +8,20 @@ interface IUserCardProps {
   data: IUserCardData;
 }
 export default function UserCard(props: IUserCardProps) {
+  const {
+    name,
+    surname,
+    email,
+    gender,
+    country,
+    birthday,
+    delivery,
+    zip,
+    consent,
+    installBrowsers,
+    notifications,
+  } = props.data;
+
   return (
     <div className={styles.card}>
       <header className={styles.card__header}>
@@ -16,31 +30,31 @@ export default function UserCard(props: IUserCardProps) {
         </div>
         <div>
           <h4 className={styles.card__title}>
-            name surname
-            <span className="material-symbols-outlined">female</span>
+            {name} {surname}
+            <span className="material-symbols-outlined">{gender}</span>
           </h4>
-          <p className={styles.card__subtitle}>email@email.com</p>
-          <p className={styles.card__subtitle}>Country</p>
+          <p className={styles.card__subtitle}>{email}</p>
+          <p className={styles.card__subtitle}>{country}</p>
         </div>
       </header>
       <div className={styles.card__body}>
         <div className={styles.card__line}>
-          <span>Birthday:</span> <span>11.11.2011</span>
+          <span>Birthday:</span> <span>{new Date(birthday).toLocaleDateString()}</span>
         </div>
         <hr />
 
         <h4 className={styles.card__title}>Delivery</h4>
         <div className={styles.card__line}>
-          <span>Zip-code:</span> <span>111-111</span>
+          <span>Zip-code:</span> <span>{zip}</span>
         </div>
         <div className={styles.card__line}>
-          <span>Date:</span> <span>11.11.2022</span>
+          <span>Date:</span> <span>{new Date(delivery).toLocaleDateString()}</span>
         </div>
         <hr />
         <h4 className={styles.card__title}>Permissions</h4>
-        <LineWithAllowIcon title="Consent" allow={true} />
-        <LineWithAllowIcon title="Install browsers" allow={false} />
-        <LineWithAllowIcon title="Notifications" allow={false} />
+        <LineWithAllowIcon title="Consent" allow={consent} />
+        <LineWithAllowIcon title="Install browsers" allow={installBrowsers} />
+        <LineWithAllowIcon title="Notifications" allow={notifications} />
       </div>
     </div>
   );
