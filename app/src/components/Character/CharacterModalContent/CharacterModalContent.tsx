50d51824a09eb0b0a3e617a61a864d6b7d58af2c
Wsko-12 <vaskovladislav@gmail.com>
2022-10-16 16:03:32 +0300

    feat: add modal content

--- app/src/components/Character/CharacterModalContent/CharacterModalContent.tsx
+++ app/src/components/Character/CharacterModalContent/CharacterModalContent.tsx
@@ -1,12 +1,40 @@
 import React, { memo } from 'react';
 import { ICharacter } from 'ts/interfaces';
+import styles from './character-modal.module.css';
 
 interface ICharacterModalContentProps {
   data: ICharacter;
 }
 
+const CardLine = memo<{ title: string; children: React.ReactNode }>(({ title, children }) => {
+  return (
+    <div className={styles.line}>
+      <span>{title}</span>
+      <span>{children}</span>
+    </div>
+  );
+});
+
 const CharacterModalContent = memo<ICharacterModalContentProps>(({ data }) => {
-  return <div>{data.name}</div>;
+  const {
+    image,
+    name,
+    status,
+    gender,
+    species,
+    origin: { name: origin },
+  } = data;
+  return (
+    <div className={styles.container}>
+      <h3 className={styles.title}>{name}</h3>
+      <img src={image} alt={data.name} />
+      <span>
+        {species} | {gender}
+      </span>
+      <CardLine title="origin">{origin}</CardLine>
+      <CardLine title="status">{status}</CardLine>
+    </div>
+  );
 });
 
 export default CharacterModalContent;
